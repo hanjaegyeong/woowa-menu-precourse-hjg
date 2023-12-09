662e25a2bacb441f8662270692fd00b26b739e68
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-12-09 16:48:35 +0900

    feat(domain): CoachNameParser 상수화

--- src/main/java/menu/domain/inputParser/CoachNameParser.java
+++ src/main/java/menu/domain/inputParser/CoachNameParser.java
@@ -5,6 +5,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CoachNameParser {
+    public static final int MIN_COACHES = 2;
+    public static final int MAX_COACHES = 5;
+    public static final int MIN_NAME_LENGTH = 2;
+    public static final int MAX_NAME_LENGTH = 4;
+
+    public static final String ERROR_MIN_MAX_COACHES = "[ERROR] 코치는 최소 " + MIN_COACHES + "명 이상, 최대 " + MAX_COACHES + "명까지만 입력 가능합니다.";
+    public static final String ERROR_NAME_LENGTH = "[ERROR] 각 코치의 이름은 최소 " + MIN_NAME_LENGTH + "글자, 최대 " + MAX_NAME_LENGTH + "글자여야 합니다.";
+
     public List<String> parseNames(String input) {
         List<String> names = Arrays.stream(input.split(","))
                 .map(String::trim)
@@ -19,17 +27,16 @@ public class CoachNameParser {
         validateNameLengths(names);
     }
 
-
     private void validateNumberOfCoaches(List<String> names) {
-        if (names.size() < 2 || names.size() > 5) {
-            throw new IllegalArgumentException("코치는 최소 2명 이상, 최대 5명까지만 입력 가능합니다.");
+        if (names.size() < MIN_COACHES || names.size() > MAX_COACHES) {
+            throw new IllegalArgumentException(ERROR_MIN_MAX_COACHES);
         }
     }
 
     private void validateNameLengths(List<String> names) {
         for (String name : names) {
-            if (name.length() < 2 || name.length() > 4) {
-                throw new IllegalArgumentException("각 코치의 이름은 최소 2글자, 최대 4글자여야 합니다.");
+            if (name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH) {
+                throw new IllegalArgumentException(ERROR_NAME_LENGTH);
             }
         }
     }
