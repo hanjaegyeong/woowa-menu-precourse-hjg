930b6fe6dc75aad57272058210f18cdef7994a2a
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-12-09 16:50:51 +0900

    feat(domain): Application 클래스 컨트롤러 추가

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,10 @@
 package menu;
 
+import menu.controller.MenuRecommendationController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        MenuRecommendationController menuRecommendationController = new MenuRecommendationController();
+        menuRecommendationController.startRecommendationProcess();
     }
 }
