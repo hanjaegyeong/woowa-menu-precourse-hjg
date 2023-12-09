2bfbdb0120c286b1ec796cfe3472a19b4e46b224
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-12-09 14:25:20 +0900

    feat(domain): CategoryRecommender 클래스에 주중카테고리들 추천 기능 추가

--- src/main/java/menu/domain/recommender/CategoryRecommender.java
+++ src/main/java/menu/domain/recommender/CategoryRecommender.java
@@ -3,6 +3,8 @@ package menu.domain.recommender;
 import camp.nextstep.edu.missionutils.Randoms;
 import menu.domain.MenuCategory;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -15,6 +17,23 @@ public class CategoryRecommender {
         CATEGORY_MAP.put(3, MenuCategory.CHINESE);
         CATEGORY_MAP.put(4, MenuCategory.ASIAN);
         CATEGORY_MAP.put(5, MenuCategory.WESTERN);
+    }
+
+    public List<MenuCategory> recommendWeeklyCategories() {
+        List<MenuCategory> weeklyCategories = new ArrayList<>();
+        Map<MenuCategory, Integer> categoryCount = new HashMap<>();
+
+        while (weeklyCategories.size() < 5) {
+            MenuCategory category = recommendCategory();
+            categoryCount.putIfAbsent(category, 0);
+
+            if (categoryCount.get(category) < 2) {
+                categoryCount.put(category, categoryCount.get(category) + 1);
+                weeklyCategories.add(category);
+            }
+        }
+
+        return weeklyCategories;
     }
 
     public MenuCategory recommendCategory() {
