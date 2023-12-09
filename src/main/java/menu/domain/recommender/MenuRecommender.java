cdd800255671cc2932ab1314f4deeb2b69f443cc
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-12-09 16:49:09 +0900

    feat(domain): MenuRecommender 클래스 recommendMenu() 매서드 반환타입 변경

--- src/main/java/menu/domain/recommender/MenuRecommender.java
+++ src/main/java/menu/domain/recommender/MenuRecommender.java
@@ -7,14 +7,13 @@ import menu.domain.Menu;
 import java.util.List;
 
 public class MenuRecommender {
-    public void recommendMenu(Coach coach, Menu category) {
+    public String recommendMenu(Coach coach, Menu category) {
         List<String> menus = category.getMenus();
 
         while(true){
             String shuffledMenu = Randoms.shuffle(menus).get(0);
             if(coach.canAddMenu(shuffledMenu)){
-                coach.addMenu(shuffledMenu);
-                return;
+                return shuffledMenu;
             }
         }
     }
