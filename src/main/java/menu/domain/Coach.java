4f02a63dfb8488a911e03351b96ad4fab3a29919
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-12-09 14:27:15 +0900

    feat(domain): Coach 클래스에 코치 생성, 추천메뉴 추가 기능 추가

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,13 +1,30 @@
 package menu.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Coach {
     private String name;
     private Set<String> restrictedMenus;
+    private List<String> recommendedMenus;
 
     public Coach(String name, Set<String> restrictedMenus) {
         this.name = name;
         this.restrictedMenus = restrictedMenus;
+        this.recommendedMenus = new ArrayList<>();
+    }
+
+    public static Coach createCoach(String name, String restrictedMenuString) {
+        Set<String> restrictedMenus = Arrays.stream(restrictedMenuString.split(","))
+                .filter(menu -> !menu.isEmpty())
+                .collect(Collectors.toSet());
+        return new Coach(name, restrictedMenus);
+    }
+
+    public void addRecommendedMenu(String menu) {
+        recommendedMenus.add(menu);
     }
 }
